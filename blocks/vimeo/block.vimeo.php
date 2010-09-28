ab411f3b10b105390206c179c2dba019517561e9
Adam Fairholm <adam.fairholm@gmail.com>
2010-09-28 14:59:27 -0400

    URL parser for Vimeo embed.

--- blocks/vimeo/block.vimeo.php
+++ blocks/vimeo/block.vimeo.php
@@ -54,14 +54,47 @@ class block_vimeo
 	 */
 	function render( $block_data )
 	{
-		if( empty($block_data) )
-			return "Vimeo Embed";
+		$video_id = $this->clean_vimeo_input( $block_data['video_id'] );
+		
+		if( ! $video_id ):
+			return "Error";
 	
-		return '<iframe src="http://player.vimeo.com/video/'.$block_data['video_id'].'" width="'.$block_data['width'].'" height="'.$block_data['height'].'" frameborder="0"></iframe>';
+		return '<iframe src="http://player.vimeo.com/video/'.$video_id.'" width="'.$block_data['width'].'" height="'.$block_data['height'].'" frameborder="0"></iframe>';
 	}
 
 	// --------------------------------------------------------------------
+
+	function clean_vimeo_input( $input )
+	{
+		// Did they just give the ID? Cool. Our work here is done.
+		
+		if( is_numeric($input) ):
+		
+			return $input;
+		
+		endif;
 	
+		// Find and return the URL:
+		
+		$url = parse_url($input);
+	
+		if( isset($url['path']) ):
+		
+			$segs = explode('/', $url['path']);
+		
+			if( is_numeric($segs[1]) ):
+			
+				return $segs[1];
+			
+			endif;
+		
+		else:
+		
+			return FALSE;
+		
+		endif;
+	
+	}	
 }
 
 /* End of file block.vimeo.php */
